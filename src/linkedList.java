1ed2a2fd7136961312bac23d57d2ed9d36c3ed24
Daria <dariakuptsova15@gmail.com>
2021-10-17 19:14:37 +0300

    merge two sorted list task and linked list initialisation

--- src/linkedList.java
+++ src/linkedList.java
@@ -1,2 +1,42 @@
 public class linkedList {
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) {this.val = val;};
+        ListNode(int val, ListNode next){this.val = val;this.next = next;}
+    }
+    static ListNode newNode(int val){
+        ListNode node = new ListNode();
+        node.val = val;
+        node.next = null;
+        return (node);
+    }
+
+    public static void addBack(ListNode a, int data){
+        ListNode last;
+        ListNode tmp;
+        tmp = new ListNode();
+        while (a.next != null)
+            a = a.next;
+        last = a;
+        tmp.val = data;
+        tmp.next = null;
+        last.next = tmp;
+    }
+
+    public static void printList(ListNode a){
+        while (a != null){
+            System.out.printf("%d ", a.val);
+            a = a.next;
+        }
+    }
+    public static void main(String[] args){
+        ListNode l1 = new ListNode();
+        l1 = newNode(1);
+        addBack(l1, 2);
+        addBack(l1, 3);
+        printList(l1);
+    }
+
 }
--- src/mergeTwoSortedList.java
+++ src/mergeTwoSortedList.java
@@ -1,12 +1,36 @@
 import java.util.*;
 public class mergeTwoSortedList {
-   public static class ListNode {
+   static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
         }
+    static ListNode newNode(int val){
+        ListNode node = new ListNode();
+        node.val = val;
+        node.next = null;
+        return (node);
+    }
+
+    public static void addBack(ListNode a, int data){
+        ListNode last;
+        ListNode tmp;
+        tmp = new ListNode();
+        while (a.next != null)
+            a = a.next;
+        last = a;
+        tmp.val = data;
+        tmp.next = null;
+        last.next = tmp;
+    }
+    public static void printList(ListNode a){
+        while (a != null){
+            System.out.printf("%d ", a.val);
+            a = a.next;
+        }
+    }
     public static ListNode merge_sorted(ListNode l1, ListNode l2) {
 
         // if both lists are empty then merged list is also empty
@@ -34,35 +58,31 @@ public class mergeTwoSortedList {
                 temp = l2;
                 l2 = l2.next;
             }
-
             mergedTail.next = temp;
             mergedTail = temp;
         }
-
         if (l1 != null) {
             mergedTail.next = l1;
         } else if (l2 != null) {
             mergedTail.next = l2;
         }
-
         return mergedHead;
-
     }
+
     public static void main(String[] args) {
-        int[] arr1 = new int[] {1, 3, 5, 6};
-        int[] arr2 = new int[] {2, 4, 6, 20, 34};
-        ListNode list_head1 = LinkedList.create_linked_list(arr1);
-        System.out.print("Original1: ");
-        LinkedList.display(list_head1);
+       ListNode l1 = new ListNode();
+       l1 = newNode(1);
+       addBack(l1, 2);
+       addBack(l1,3);
+       addBack(l1, 4);
+       ListNode l2 = new ListNode();
+       l2 = newNode(1);
+       addBack(l2, 2);
+       addBack(l2,5);
+       addBack(l2, 6);
+       ListNode res = merge_sorted(l1,l2);
+       printList(res);
 
-        LinkedListNode list_head2 = LinkedList.create_linked_list(arr2);
-        System.out.print("Original2: ");
-        LinkedList.display(list_head2);
-
-        System.out.println("\nMerged:");
-
-        LinkedListNode newHead = merge_sorted(list_head1, list_head2);
-        LinkedList.display(newHead);
     }
 }
 
